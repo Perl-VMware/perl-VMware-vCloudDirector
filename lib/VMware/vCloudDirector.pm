8f0b469a7d31e03857c813fadf97c5438d2e5148
Nigel Metheringham <nigel.metheringham@redcentricplc.com>
2017-01-12 14:32:14 +0000

    Initial framework

---
+++ lib/VMware/vCloudDirector.pm
@@ -0,0 +1,20 @@
+package VMware::vCloudDirector;
+
+# ABSTRACT: Module to do stuff!
+
+use strict;
+use warnings;
+
+# VERSION
+# AUTHORITY
+
+use Moose;
+use Method::Signatures;
+
+# ------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------
+
+__PACKAGE__->meta->make_immutable;
+
+1;
