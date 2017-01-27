606fc62f6418447e494eafa43bc78ded768e3775
Nigel Metheringham <nigel.metheringham@redcentricplc.com>
2017-01-27 15:15:08 +0000

    Change type handling
    Same mechanism as used in Object plus also now decoded at Link creation time rather than dynamically on use.

--- lib/VMware/vCloudDirector/Link.pm
+++ lib/VMware/vCloudDirector/Link.pm
@@ -28,23 +28,7 @@ has mime_type => ( is => 'ro', isa => 'Str', predicate => 'has_mime_type' );
 has href => ( is => 'ro', isa => Uri, required => 1, coerce => 1 );
 has rel  => ( is => 'ro', isa => 'Str', required  => 1 );
 has name => ( is => 'ro', isa => 'Str', predicate => 'has_name' );
-has type => ( is => 'ro', isa => 'Str', lazy      => 1, builder => '_build_type' );
-
-method _build_type () {
-    VMware::vCloudDirector::Error->throw("Type not set") unless ( $self->has_mime_type );
-
-    my $type = $self->mime_type;
-    if ( $type =~ m|^\Qapplication/vnd.vmware.vcloud.query.\E(.*)\Q+xml\E$| ) {
-        return ( 'Query/' . $1 );
-    }
-    elsif ( $type =~ m|^\Qapplication/vnd.vmware.admin.\E(.*)\Q+xml\E$| ) {
-        return ( 'Admin/' . $1 );
-    }
-    elsif ( $type =~ m|^\Qapplication/vnd.vmware.vcloud.\E(.*)\Q+xml\E$| ) {
-        return ($1);
-    }
-    VMware::vCloudDirector::Error->throw("Type $type does not match expected pattern");
-}
+has type => ( is => 'ro', isa => 'Str' );
 
 # ------------------------------------------------------------------------
 around BUILDARGS => sub {
@@ -54,10 +38,14 @@ around BUILDARGS => sub {
 
     if ( $params->{hash} ) {
         my $hash = delete $params->{hash};
-        $params->{href}      = $hash->{-href} if ( exists( $hash->{-href} ) );
-        $params->{rel}       = $hash->{-rel}  if ( exists( $hash->{-rel} ) );
-        $params->{name}      = $hash->{-name} if ( exists( $hash->{-name} ) );
-        $params->{mime_type} = $hash->{-type} if ( exists( $hash->{-type} ) );
+        $params->{href} = $hash->{-href} if ( exists( $hash->{-href} ) );
+        $params->{rel}  = $hash->{-rel}  if ( exists( $hash->{-rel} ) );
+        $params->{name} = $hash->{-name} if ( exists( $hash->{-name} ) );
+        if ( exists( $hash->{-type} ) ) {
+            my $type = $hash->{-type};
+            $params->{mime_type} = $type;
+            $params->{type} = $1 if ( $type =~ m|^application/vnd\..*\.(\w+)\+xml$| );
+        }
     }
 
     return $class->$orig($params);
